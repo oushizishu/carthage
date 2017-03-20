70552981ab8c988245815f7efe1400094edc1d26
辛亚鹏 <xinyapeng@baijiahulian.com>
2017-03-20 15:50:45 +0800

    no message

---
+++ YPCarthage/YPCarthage.h
@@ -0,0 +1,19 @@
+//
+//  YPCarthage.h
+//  YPCarthage
+//
+//  Created by 辛亚鹏 on 2017/3/20.
+//  Copyright © 2017年 xinyapeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YPCarthage.
+FOUNDATION_EXPORT double YPCarthageVersionNumber;
+
+//! Project version string for YPCarthage.
+FOUNDATION_EXPORT const unsigned char YPCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YPCarthage/PublicHeader.h>
+
+
