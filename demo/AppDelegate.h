9f90edfebaabeb62a262a7c054645697fe7f9ba1
辛亚鹏 <xinyapeng@baijiahulian.com>
2017-03-20 15:43:42 +0800

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 辛亚鹏 on 2017/3/20.
+//  Copyright © 2017年 xinyapeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
